8528816b81b333dac5f047ce4798f3c1cc744364
githubfacu <elorzfacundo@gmail.com>
2026-05-09 01:21:51 -0300

    feat: actividades

---
+++ estructurada/16-js-estructurada/continents.js
@@ -0,0 +1,12 @@
+const { ask, rl } = require('../../utils/readline');
+
+const { SUBREGIONS } = require('./data/subregions')
+const { REGIONS } = require('./data/regions')
+
+async function continentsApp(){
+
+    
+    rl.close();
+}
+
+continentsApp()
--- estructurada/16-js-estructurada/countries.js
+++ estructurada/16-js-estructurada/countries.js
@@ -1,5 +1,33 @@
 const { ask, rl } = require('../../utils/readline');
 
-const NAMES = require('./names')
+const { NAMES : countries } = require('./data/names')
 
-console.log(NAMES);
+async function countriesApp(){
+
+    const OPTIONS_MENU = '1. Ver países\n' + '2. Buscar país\n' + '3. Salir\n' + 'Escoge opción? [1-3]: '
+    let optionSelected
+    
+    do {
+        switch (+optionSelected) {
+            case 1:
+                console.log(countries);
+                break;
+            case 2:
+                let countryToSearch = await ask('Dame el nombre del país: ')
+                let isCountryIncluded = false
+                for (let i = 0; !isCountryIncluded && i < countries.length; i++) {
+                    isCountryIncluded = countries[i] === countryToSearch
+                }
+                console.log(`El país ${countryToSearch} ${isCountryIncluded ? 'si' : 'no'} está incluido`);
+                break;
+            default:
+                break;
+        }        
+        optionSelected = await ask(OPTIONS_MENU)
+    } while (optionSelected != 3);
+
+    console.log('Fin de la aplicación');
+    rl.close();
+}
+
+countriesApp()
